fab4a752f4d86c55f15028bb8d9026f2e5c640ba
Martin Monnier <martin.monnier@compori.com>
2018-10-04 15:19:31 +0200

    Add base project

---
+++ src/Compori/StringExtensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compori.StringExtensions
+{
+    public static class StringExtensions
+    {
+
+#if NET35
+        public static bool IsNullOrWhiteSpace(this String instance)
+        {
+            return String.IsNullOrEmpty(instance) || instance.Trim().Length == 0;
+        }
+#endif
+
+    }
+}
